987769be7c527db01f8bdbaf1f4745819bad2051
Pedro Soriano Ribeiro <psorianoribeir00@mylangara.ca>
2023-09-05 19:14:37 -0300

    routes done and controller started

--- config/routes.rb
+++ config/routes.rb
@@ -1,24 +1,16 @@
 Rails.application.routes.draw do
-  get 'reviews/create'
-  get 'job_applicatiion/create'
-  get 'job_applicatiion/update'
-  get 'employees/create'
-  get 'employees/destroy'
-  get 'jobs/index'
-  get 'jobs/show'
-  get 'jobs/new'
-  get 'jobs/create'
-  get 'jobs/destroy'
-  get 'properties/index'
-  get 'properties/new'
-  get 'properties/create'
-  get 'properties/edit'
-  get 'properties/update'
-  get 'properties/destroy'
   devise_for :users
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :properties, only: %i[index new create edit update destroy] do
+    resources :jobs, only: %i[index show new create]
+    resources :employees, only: %i[create destroy]
+  end
 
+  resources :jobs, only: %i[index show] do
+    resources :japplications, only: %i[create update]
+    resources :reviews, only: %i[create]
+  end
   # Defines the root path route ("/")
   # root "articles#index"
 end
