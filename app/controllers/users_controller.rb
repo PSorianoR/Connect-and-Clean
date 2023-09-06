8d9f9f876a7ff4aa1f304a0af34506c6e4662b9d
Humberto Pedra <hphumbertopedra@yahoo.com.br>
2023-09-06 12:36:30 -0300

    profile page not finished yet

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,13 @@
+class UsersController < ApplicationController
+  before_action :set_user, only: %i[show]
+
+  def show
+    # You can customize this action to display the user's information and other related data.
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+end
