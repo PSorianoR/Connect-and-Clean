1e84fa14f07978ebc9ea6c147a159bd707ce4ec2
Pedro Soriano Ribeiro <psorianoribeir00@mylangara.ca>
2023-09-06 11:45:41 -0300

    Models validations

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,7 @@
 class Review < ApplicationRecord
   belongs_to :user
   belongs_to :job
+
+  validates :rating, presence: true, numericality: { greater_than: 0, less_than_or_equal_to: 5 }
+  validates :description, presence: true, length: { minimum: 10 }
 end
--- app/models/team.rb
+++ app/models/team.rb
@@ -1,4 +1,6 @@
 class Team < ApplicationRecord
   belongs_to :user
   belongs_to :property
+
+  validates :type, presence: true
 end
