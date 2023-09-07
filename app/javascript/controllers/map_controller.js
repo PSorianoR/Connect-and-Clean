b5f0a52afbcba553b5a16f657d7ea2a8929befad
Matthew McGovern <mattmcg37@gmail.com>
2023-09-07 20:28:22 -0300

    geocode update 2

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -14,6 +14,24 @@ export default class extends Controller {
     this.map = new mapboxgl.Map({
       container: this.element,
       style: "mapbox://styles/mapbox/streets-v11",
+    });
+    this.#addMarkersToMap()
+    this.#fitMapToMarkers()
+  }
+
+  #fitMapToMarkers() {
+    const bounds = new mapboxgl.LngLatBounds()
+    this.markersValue.forEach((marker) => {
+      bounds.extend([marker.lng, marker.lat])
+    })
+    this.map.fitBounds(bounds, {padding: 50, duration: 150})
+  }
+
+  #addMarkersToMap() {
+    this.markersValue.forEach((marker) => {
+      new mapboxgl.Marker()
+      .setLngLat([marker.lng, marker.lat])
+      .addTo(this.map);
     })
   }
 }
