4f09b4a55ace2d156a8791e73f71b075ca4b008f
Humberto Pedra <hphumbertopedra@yahoo.com.br>
2023-09-08 10:32:08 -0300

    not implemented yet

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,4 +1,23 @@
 class ReviewsController < ApplicationController
-  def create
+  def new
+    @review = Review.new
   end
+
+  def create
+    @review = Review.new(review_params)
+    @review.user = current_user
+    if @review.save
+      redirect_to new_job_review_path
+    else
+      flash[:alert] = "Something went wrong."
+      render :new
+    end
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:description, :rating)
+  end
+
 end
